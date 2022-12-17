8593260af2467378b18f91d32d66f02f851d4743
Samuele Peirone <samuelepeirone2001@gmail.com>
2022-12-17 22:41:45 +0100

    nuova registrazione

---
+++ src/jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    //REGISTRAZIONE.HTML
+    $('#submit_registrazione').click(function () {
+        $("#form_registrazione").hide(300);
+        $("#check_validazione").show(300);
+        $("#check_validazione i").show(1000);
+        setTimeout(function(){
+            $("#redirectmessage").fadeToggle(1000);
+        }, 4000);
+        setTimeout(function(){
+            window.location.replace("index.html");
+        }, 10000);
+
+    });
+});
