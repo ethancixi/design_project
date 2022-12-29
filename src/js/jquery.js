31160dc10e2fc1e53fea81461527b83f028fa1cd
Samuele Peirone <samuelepeirone2001@gmail.com>
2022-12-29 18:38:55 +0100

    accessibikità fino a slide n.40 fatta

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -49,21 +49,26 @@ function caroselloimmagini() {
         "Crediamo nelle persone come mezzo di diffusione di idee e soluzioni a problematiche",
         "Ci spingiamo oltre i nostri limiti per difendere ogni giorno ciò a cui più teniamo: il nostro pianeta",
         "Costruiamo ponti fra persone, per garantire una solida diffusione delle tematiche che weAct supporta"];
+    const arrayalt = ["Ingresso di un teatro, la cui insegna recita 'Dreamland', la Città dei sogni in Inglese", "Vista dall'alto di un deserto, dal quale si innalzano un grandissimo numero di mongolfiere colorate.",
+        "Immagine di una folla ad un concerto, in movimento e con le mani alzate", "Vista di un edificio posto su una scogliera, sulla parte più esterna del quale si abbattono le onde",
+        "Ponte di legno in mezzo ad un fitto bosco di grandi alberi."];
     $("#imgcarosello").attr("src", arraysources[0]);
+    $("#imgcarosello").attr("alt", arrayalt[0]);
     $("#pcarosello").html(arrayparagraphs[0]);
-    $("#indicecarosello").html("1/"+arraysources.length);
+    $("#indicecarosello").html("1/" + arraysources.length);
     $("#avanti").click(function () {
         var src = $("#imgcarosello").attr("src");
         var posizione = arraysources.indexOf(src);
         $("#indietro").prop("disabled", false);
         if (posizione == arraysources.length - 2) {
             $("#avanti").prop("disabled", true);
-            
+
         }
         posizione++;
         $("#imgcarosello").attr("src", arraysources[posizione]);
+        $("#imgcarosello").attr("alt", arrayalt[posizione]);
         $("#pcarosello").html(arrayparagraphs[posizione]);
-        $("#indicecarosello").html((posizione+1)+"/"+arraysources.length);
+        $("#indicecarosello").html((posizione + 1) + "/" + arraysources.length);
     })
     $("#indietro").click(function () {
         var src = $("#imgcarosello").attr("src");
@@ -74,11 +79,12 @@ function caroselloimmagini() {
         }
         posizione--;
         $("#imgcarosello").attr("src", arraysources[posizione]);
+        $("#imgcarosello").attr("alt", arrayalt[posizione]);
         $("#pcarosello").html(arrayparagraphs[posizione]);
-        $("#indicecarosello").html((posizione+1)+"/"+arraysources.length);
+        $("#indicecarosello").html((posizione + 1) + "/" + arraysources.length);
     })
 }
 //ABOUTUS
-function mostramappa(){
+function mostramappa() {
     $("#mappa").toggle(400);
 }
